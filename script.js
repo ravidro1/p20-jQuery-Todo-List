c4a79ad781ad599b6fde9080c9574e78b5075e0f
ravidro1 <ravidrrt@gmail.com>
2023-08-06 20:33:23 +0300

    finish: functionality

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 $(document).ready(() => {
-  const tasksContainer = $("#tasks-container");
-  const itemTemplate = $("#item-template")[0];
-  const addTaskButton = $("#add-task-button");
-  const newTaskInput = $("#new-task-input");
-
-  const listsContainer = $("#lists-container");
-  const listTemplate = $("#list-template")[0];
-  const addListButton = $("#add-list-button");
-  const newListInput = $("#new-list-input");
-
-  let lists = getLists();
-  //   refreshList();
-
   // localStorage functions
   function setLists(arg_lists) {
     const stringifyLists = JSON.stringify(arg_lists);
@@ -23,112 +10,41 @@ $(document).ready(() => {
     return JSON.parse(localItems);
   }
 
-  // task functions
-  //   function addItem(description) {
-  //     items.unshift({
-  //       checked: false,
-  //       description,
-  //     });
+  const listsContainer = $("#lists-container");
+  const listTemplate = $("#list-template")[0];
+  const addListButton = $("#add-list-button");
+  const newListInput = $("#new-list-input");
 
-  //     setItems(items);
-  //     refreshList();
-  //   }
+  const tasksContainer = $("#tasks-container");
+  const taskTemplate = $("#task-template")[0];
+  const addTaskButton = $("#add-task-button");
+  const newTaskInput = $("#new-task-input");
 
-  //   function deleteItem(index) {
-  //     items = items.filter((_, i) => index != i);
-  //     setItems(items);
-  //     refreshList();
-  //   }
+  let lists = getLists();
+  let currentListIndex = null;
+  const getCurrentList = () => {
+    if (currentListIndex == null) return null;
+    return lists[currentListIndex].list;
+  };
 
-  //   function updateItem(item, key, value) {
-  //     item[key] = value;
-  //     setItems(items);
-  //     refreshList();
-  //   }
+  renderAllLists();
 
-  function renderTasks(list) {
-    tasksContainer.empty();
+  function listChanged() {
+    const currentList = getCurrentList();
 
-    list.list.forEach((item, index) => {
-      const itemElement = itemTemplate.content.cloneNode(true);
-
-      const checkedElement = itemElement.querySelector(".item-checkbox");
-      const descriptionElement = itemElement.querySelector(".item-text");
-      const editInputElement = itemElement.querySelector(".item-edit-input");
-      const editButtonElement = itemElement.querySelector(".item-edit-button");
-      const deleteButtonElement = itemElement.querySelector(
-        ".item-delete-button"
-      );
-
-      checkedElement.checked = item.checked;
-      descriptionElement.innerText = item.description;
-
-      checkedElement.onchange = (e) => {
-        const isChecked = e.target.checked;
-        updateItem(item, "checked", isChecked);
-      };
-
-      let isInEditMode = false;
-      let editInputValue = "";
-
-      editButtonElement.onclick = () => {
-        isInEditMode = !isInEditMode;
-
-        if (isInEditMode) {
-          $(descriptionElement).css({ display: "none" });
-          $(editInputElement).css({ display: "block" });
-          $(editInputElement).on("input", (e) => {
-            editInputValue = e.target.value;
-          });
-        } else {
-          $(descriptionElement).css({ display: "block" });
-          $(editInputElement).css({ display: "none" });
-          if (editInputValue) {
-            updateItem(item, "description", editInputValue);
-          }
-
-          editInputValue = "";
-          $(editInputElement).empty();
-        }
-
-        $(editInputElement).val(item.description);
-        console.log(isInEditMode);
-      };
-
-      deleteButtonElement.onclick = () => {
-        deleteItem(index);
-      };
-
-      if (item.checked) {
-        $(descriptionElement).css({ "text-decoration-line": "line-through" });
-      }
-
-      tasksContainer.append(itemElement);
-    });
-  }
-
-  // *****************************************
-
-  // tasks Events
-  let newTaskInputValue = "";
-
-  newTaskInput.on("input", (e) => {
-    newTaskInputValue = e.target.value;
-  });
-
-  addTaskButton.on("click", () => {
-    if (!newTaskInputValue) {
-      alert("Input can not be empty");
+    if (currentList == null) {
+      newTaskInput.css({ display: "none" });
+      addTaskButton.css({ display: "none" });
       return;
     }
-    addItem(newTaskInputValue);
-    newTaskInputValue = "";
-    newTaskInput.val("");
-  });
-  // *****************************************
+
+    newTaskInput.css({ display: "block" });
+    addTaskButton.css({ display: "block" });
+
+    renderTasks(currentList);
+  }
 
   // list functions
-
   function addList(name) {
     lists.unshift({ name, list: [] });
     renderAllLists();
@@ -141,10 +57,10 @@ $(document).ready(() => {
     setLists(lists);
   }
 
-  function updateListData() {}
-
-  let currentListIndex = null;
-  //   currentListIndex
+  function updateListName(list, newName) {
+    list.name = newName;
+    setLists(lists);
+  }
 
   function renderAllLists() {
     listsContainer.empty();
@@ -167,9 +83,10 @@ $(document).ready(() => {
       listNameElement.innerText = list?.name;
 
       let isListInEditMode = false;
+
       $(listEditButtonElement).on("click", () => {
         isListInEditMode = !isListInEditMode;
-
+        
         if (isListInEditMode) {
           $(listEditInputElement).val(list?.name);
           $(listEditInputElement).css({ display: "block" });
@@ -177,7 +94,7 @@ $(document).ready(() => {
         } else {
           const inputValue = $(listEditInputElement).val();
           if (inputValue) {
-            // updateListData()
+            updateListName(lists[index], inputValue);
           }
           $(listEditInputElement).css({ display: "none" });
           $(listNameElement).css({ display: "block" });
@@ -187,14 +104,15 @@ $(document).ready(() => {
 
       $(listDeleteButtonElement).on("click", () => {
         deleteList(index);
+        currentListIndex = null;
+        listChanged();
       });
 
       $(listNameElement)
         .parent()
         .on("click", () => {
           currentListIndex = index;
-          //   renderTasks(lists[currentListIndex]);
-          //   console.log(index);
+          listChanged();
         });
 
       listsContainer.append(listElement);
@@ -220,10 +138,109 @@ $(document).ready(() => {
 
   // *****************************************
 
-  // const listsContainer = $("#lists-container");
-  // const listTemplate = $("#list-template")[0];
-  // const addListButton = $("#add-list-button");
-  // const newListInput = $("#new-list-input");
+  // task functions
+  function addTask(description) {
+    const currentList = getCurrentList();
+    currentList.unshift({
+      checked: false,
+      description,
+    });
 
-  renderAllLists();
+    setLists(lists);
+    renderTasks(currentList);
+  }
+
+  function deleteTask(index) {
+    lists[currentListIndex].list = getCurrentList().filter(
+      (_, i) => index != i
+    );
+    setLists(lists);
+    renderTasks(getCurrentList());
+  }
+
+  function updateTask(task, key, value) {
+    task[key] = value;
+    setLists(lists);
+    renderTasks(getCurrentList());
+  }
+
+  function renderTasks(list) {
+    tasksContainer.empty();
+
+    list.forEach((task, index) => {
+      const taskElement = taskTemplate.content.cloneNode(true);
+
+      const checkedElement = taskElement.querySelector(".task-checkbox");
+      const descriptionElement = taskElement.querySelector(".task-text");
+      const editInputElement = taskElement.querySelector(".task-edit-input");
+      const editButtonElement = taskElement.querySelector(".task-edit-button");
+      const deleteButtonElement = taskElement.querySelector(
+        ".task-delete-button"
+      );
+
+      checkedElement.checked = task.checked;
+      descriptionElement.innerText = task.description;
+
+      checkedElement.onchange = (e) => {
+        const isChecked = e.target.checked;
+        updateTask(task, "checked", isChecked);
+      };
+
+      let isInEditMode = false;
+      let editInputValue = "";
+
+      editButtonElement.onclick = () => {
+        isInEditMode = !isInEditMode;
+
+        if (isInEditMode) {
+          $(descriptionElement).css({ display: "none" });
+          $(editInputElement).css({ display: "block" });
+          $(editInputElement).on("input", (e) => {
+            editInputValue = e.target.value;
+          });
+        } else {
+          $(descriptionElement).css({ display: "block" });
+          $(editInputElement).css({ display: "none" });
+          if (editInputValue) {
+            updateTask(task, "description", editInputValue);
+          }
+
+          editInputValue = "";
+          $(editInputElement).empty();
+        }
+
+        $(editInputElement).val(task.description);
+        console.log(isInEditMode);
+      };
+
+      deleteButtonElement.onclick = () => {
+        deleteTask(index);
+      };
+
+      if (task.checked) {
+        $(descriptionElement).css({ "text-decoration-line": "line-through" });
+      }
+
+      tasksContainer.append(taskElement);
+    });
+  }
+
+  // *****************************************
+
+  // tasks Events
+  let newTaskInputValue = "";
+  newTaskInput.on("input", (e) => {
+    newTaskInputValue = e.target.value;
+  });
+
+  addTaskButton.on("click", () => {
+    if (!newTaskInputValue) {
+      alert("Input can not be empty");
+      return;
+    }
+    addTask(newTaskInputValue);
+    newTaskInputValue = "";
+    newTaskInput.val("");
+  });
+  // *****************************************
 });
